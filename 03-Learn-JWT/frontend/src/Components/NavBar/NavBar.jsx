f67c9e320558b279b27496a7092348d59c970278
thanhtuan472k <tuango.472000@gmail.com>
2022-03-02 15:06:50 +0700

    feature/done logout

--- 03-Learn-JWT/frontend/src/Components/NavBar/NavBar.jsx
+++ 03-Learn-JWT/frontend/src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './navbar.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../redux/apiRequest';
+import { createAxios } from '../../createInstance';
+import { logOutSuccess } from '../../redux/authSlice';
 
 const NavBar = () => {
   const user = useSelector(state => state.auth.login.currentUser);
+  const accessToken = user?.accessToken;
+  const id = user?._id;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  let axiosJWT = createAxios(user, dispatch, logOutSuccess);
+
+  const handleLogout = () => {
+    logout(dispatch, id, navigate, accessToken, axiosJWT);
+  };
   return (
     <nav className='navbar-container'>
       <Link to='/' className='navbar-home'>
@@ -16,7 +28,7 @@ const NavBar = () => {
           <p className='navbar-user'>
             Hi, <span> {user.username}</span>{' '}
           </p>
-          <Link to='/logout' className='navbar-logout'>
+          <Link to='/logout' className='navbar-logout' onClick={handleLogout}>
             {' '}
             Log out
           </Link>
