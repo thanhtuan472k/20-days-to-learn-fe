0475c08103513deed4bce6f55c2e649e18cd8988
thanhtuan472k <tuango.472000@gmail.com>
2022-03-02 15:02:02 +0700

    homepage logic

--- 03-Learn-JWT/frontend/src/Components/Home/HomePage.jsx
+++ 03-Learn-JWT/frontend/src/Components/Home/HomePage.jsx
@@ -1,63 +1,59 @@
 import { useEffect } from 'react';
-import { getAllUsers } from '../../redux/apiRequest';
+import { deleteUser, getAllUsers } from '../../redux/apiRequest';
 import './home.css';
+import jwt_decode from 'jwt-decode';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import axios, { Axios } from 'axios';
+import { loginSuccess } from '../../redux/authSlice';
+import { createAxios } from '../../createInstance';
 
 const HomePage = () => {
   const user = useSelector(state => state.auth.login?.currentUser);
-  // const userList = useSelector(state => state.user.users.allUsers);
+  // optional chaining
+  const userList = useSelector(state => state.users.users.allUsers);
+  const mgs = useSelector(state => state.users?.mgs);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  //DUMMY DATA
-  const userData = [
-    {
-      username: 'anhduy1202'
-    },
-    {
-      username: 'kelly123422'
-    },
-    {
-      username: 'danny5678'
-    },
-    {
-      username: 'kenny1122'
-    },
-    {
-      username: 'jack1234'
-    },
-    {
-      username: 'loi1202'
-    },
-    {
-      username: 'nhinhi2009'
-    },
-    {
-      username: 'kellynguyen1122'
-    }
-  ];
+  let axiosJWT = createAxios(user, dispatch, loginSuccess);
 
   useEffect(() => {
     if (!user) {
       navigate('/login');
     }
     if (user?.accessToken) {
-      getAllUsers(user?.accessToken, dispatch);
+      getAllUsers(user?.accessToken, dispatch, axiosJWT);
     }
   }, []);
+
+  const handleDelete = id => {
+    console.log(id);
+    deleteUser(user?.accessToken, dispatch, id);
+  };
+
   return (
     <main className='home-container'>
       <div className='home-title'>User List</div>
+      <div className='home-role'>{`Your role: ${
+        user?.admin ? `Admin` : `User`
+      }`}</div>
       <div className='home-userlist'>
-        {userData.map(user => {
+        {userList?.map(user => {
           return (
             <div className='user-container'>
               <div className='home-user'>{user.username}</div>
-              <div className='delete-user'> Delete </div>
+              <div
+                className='delete-user'
+                onClick={() => handleDelete(user._id)}
+              >
+                {' '}
+                Delete{' '}
+              </div>
             </div>
           );
         })}
       </div>
+      <div className='errMgs'>{mgs}</div>
     </main>
   );
 };
