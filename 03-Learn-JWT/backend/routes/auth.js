df46dc63f3ae9923d6e77bac4c74c8cf8d77187c
thanhtuan472k <tuango.472000@gmail.com>
2022-03-02 14:52:48 +0700

    delete folder backend

--- 03-Learn-JWT/backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const authController = require('../controllers/authControllers');
-
-const router = require('express').Router();
-
-router.post('/register', authController.registerUser);
-router.post('/login', authController.loginUser);
-
-module.exports = router;
